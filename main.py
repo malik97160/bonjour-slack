772d32f12d59eecffbeca027039b2df44bd31375
Malik Couchy <malik.coucchy@gmail.com>
2018-07-26 21:31:41 +0200

    another commit

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from slackclient import SlackClient
 from datetime import datetime
-import smtplib
-from email.mime.text import MIMEText
 
 counter = 0
 time = datetime.now()
 today10h30am = time.replace(hour=10, minute=30, second=0, microsecond=0)
 today9h45am = time.replace(hour=9, minute=45, second=0, microsecond=0)
-# and  today9h45am < time < today10h30am
-while counter < 1:
+while counter < 1 and  today9h45am < time < today10h30am:
     r = requests.get('http://dites.bonjourmadame.fr/')
     c = r.content
     soup = BeautifulSoup(c, "html.parser")
@@ -26,19 +23,5 @@ while counter < 1:
         print("prems")
         image = timeStamp.find('a').findChildren('img')[0].get('src')
         print(image)
-
-        msg = MIMEText(image)
-
-        me = 'malik.couchy@gmail.com'
-        you = 'clikclik97160@hotmail.fr'
-        msg['Subject'] = 'Test'
-        msg['From'] = me
-        msg['To'] = you
-
-        # Send the message via our own SMTP server, but don't include the
-        # envelope header.
-        s = smtplib.SMTP('localhost')
-        s.sendmail(me, [you], msg.as_string())
-        s.quit()
 
         counter +=1
