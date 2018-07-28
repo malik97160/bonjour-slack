d12ba00a7b02d55edb5b5659fc7324b43efcc850
Malik Couchy <malik.coucchy@gmail.com>
2018-07-28 23:40:11 +0200

    ajout des variables d'environnement

--- main.py
+++ main.py
@@ -4,31 +4,33 @@ from slackclient import SlackClient
 from datetime import datetime
 from slackclient import SlackClient
 import time
+import os
 
+path = os.environ['bonjourPath']
+slackToken = os.environ['slackToken']
 counter = 0
 temps = datetime.now()
 today10h30am = temps.replace(hour=10, minute=30, second=0, microsecond=0)
 today9h40am = temps.replace(hour=9, minute=40, second=0, microsecond=0)
-fichierTime = fichier2 = open('time.txt', 'a')
+fichierTime = fichier2 = open(path+'time.txt', 'a')
 fichierTime.write("script exécuté le "+str(temps))
 fichierTime.close()
-while counter < 1 and  today9h40am < temps < today10h30am:
+while counter < 1 and today9h40am <= time < today10h30am:
     r = requests.get('http://dites.bonjourmadame.fr/')
     c = r.content
     soup = BeautifulSoup(c, "html.parser")
     timeStamp = soup.find(class_= 'post')
-    href = timeStamp.find('a').get('href')
-    fichier = open('href.txt', 'r')
-    old_href = fichier.read()
+    image = timeStamp.findChildren('img')[0].get('src')
+    fichier = open(path+'href.txt', 'r')
+    old_image = fichier.read()
     fichier.close
-    if old_href != href :
-        fichier2 = open('href.txt', 'w')
-        fichier2.write(href)
+    if old_image != image :
+        fichier2 = open(path+'href.txt', 'w')
+        fichier2.write(image)
         fichier2.close()
         print("prems")
-        image = timeStamp.find('a').findChildren('img')[0].get('src')
         print(image)
-        slack_token = ""
+        slack_token = slackToken
         sc = SlackClient(slack_token)
         sc.api_call(
         "chat.postMessage",
